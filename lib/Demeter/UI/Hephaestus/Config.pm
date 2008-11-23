157ac932ce589a184955f5f7df1f144b198922c0
demeter <demeter@a48c6eca-4e1b-0410-a7b3-d802d4c683d2>
2008-11-23 21:33:26 +0000

    adding Hephaestus' Config page
    git-svn-id: http://cars9.uchicago.edu/svn/demeter/trunk@332 a48c6eca-4e1b-0410-a7b3-d802d4c683d2

---
+++ lib/Demeter/UI/Hephaestus/Config.pm
@@ -0,0 +1,98 @@
+package Demeter::UI::Hephaestus::Config;
+use strict;
+use warnings;
+use Carp;
+use Wx qw( :everything );
+
+use Demeter::UI::Wx::Config;
+
+use base 'Wx::Panel';
+
+sub new {
+  my ($class, $page, $echoarea) = @_;
+  my $self = $class->SUPER::new($page, -1, wxDefaultPosition, wxDefaultSize, wxMAXIMIZE_BOX );
+  $self->{echo} = $echoarea;
+
+  my $hbox = Wx::BoxSizer->new( wxVERTICAL );
+
+  my $config = Demeter::UI::Wx::Config->new($self);
+  $hbox -> Add($config, 1, wxEXPAND|wxALL, 5);
+
+  $self->SetSizerAndFit($hbox);
+
+  return $self;
+};
+
+
+1;
+
+=head1 NAME
+
+Demeter::UI::Hephaestus::Config - Hephaestus' electronic transitions utility
+
+=head1 VERSION
+
+This documentation refers to Demeter version 0.2.
+
+=head1 SYNOPSIS
+
+The contents of Hephaestus' electronic transistions utility can be
+added to any Wx application.
+
+  my $page = Demeter::UI::Hephaestus::Config->new($parent,$echoarea);
+  $sizer -> Add($page, 1, wxGROW|wxEXPAND|wxALL, 0);
+
+The arguments to the constructor method are a reference to the parent
+in which this is placed and a reference to a mechanism for displaying
+progress and warning messages.  The C<$echoarea> object must provide a
+method called C<echo>.
+
+C<$page> contains most of what is displayed in the main part of the
+Hephaestus frame.  Only the label at the top is not included in
+C<$page>.
+
+=head1 DESCRIPTION
+
+This utility presents a diagram explaining the electronic transitions
+associated with the various fluorescence lines.
+
+=head1 CONFIGURATION
+
+
+=head1 DEPENDENCIES
+
+Demeter's dependencies are in the F<Bundle/DemeterBundle.pm> file.
+
+=head1 BUGS AND LIMITATIONS
+
+=over 4
+
+=item *
+
+Draw the chart on a canvas and provide some interactivity along with a
+periodic table popup.
+
+=back
+
+Please report problems to Bruce Ravel (bravel AT bnl DOT gov)
+
+Patches are welcome.
+
+=head1 AUTHOR
+
+Bruce Ravel (bravel AT bnl DOT gov)
+
+L<http://cars9.uchicago.edu/~ravel/software/>
+
+=head1 LICENCE AND COPYRIGHT
+
+Copyright (c) 2006-2008 Bruce Ravel (bravel AT bnl DOT gov). All rights reserved.
+
+This module is free software; you can redistribute it and/or
+modify it under the same terms as Perl itself. See L<perlgpl>.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+=cut
