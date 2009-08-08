660e1f631dfaf3562c492d47ae89daa66d9bedb6
(no author) <(no author)@a48c6eca-4e1b-0410-a7b3-d802d4c683d2>
2009-08-08 19:23:00 +0000

    check in Histogram.pm
    git-svn-id: http://cars9.uchicago.edu/svn/demeter/trunk@504 a48c6eca-4e1b-0410-a7b3-d802d4c683d2

---
+++ lib/Demeter/ScatteringPath/Histogram.pm
@@ -0,0 +1,70 @@
+package Demeter::ScatteringPath::Histogram;
+use Moose::Role;
+
+use List::Util qw(sum);
+
+sub make_histogram {
+  my ($self, $rx, $ry, $common) = @_;
+  my @paths = ();
+
+  my $total = sum(@$ry);
+  my $rnot = $self->fuzzy;
+  foreach my $i (0 .. $#{$rx}) {
+    my $deltar = $rx->[$i] - $rnot;
+    my $amp = $ry->[$i] / $total;
+    my $this = Demeter::Path->new(sp     => $self,
+				  delr   => $deltar,
+				  n      => $amp,
+				  parent => $self->feff,
+				  @$common,
+				 );
+    $this -> make_name;
+    $this -> name($this->name . " at " . $rx->[$i]);
+    push @paths, $this;
+  };
+
+  return @paths;
+};
+
+sub histogram_from_file {
+  my ($self, $fname, $xcol, $ycol, $rmin, $rmax) = @_;
+  $xcol ||= 1;
+  $ycol ||= 2;
+  $rmin ||= 0;
+  $rmax ||= 100;
+  $xcol  -= 1;
+  $ycol  -= 1;
+  my (@x, @y);
+  return (\@x, \@y) if (not -e $fname);
+  open(my $H, $fname);
+  foreach my $line (<$H>) {
+    chomp $line;
+    next if ($line =~ m{\A\s*\z});
+    next if ($line =~ m{\A\#\*\%;});
+    my @list = split(" ", $line);
+    next if ($list[$xcol] < $rmin);
+    next if ($list[$xcol] > $rmax);
+    push @x, $list[$xcol];
+    push @y, $list[$ycol];
+  };
+  close $H;
+
+  return \@x, \@y;
+};
+
+sub histogram_from_function {
+  my ($self, $string, $rmin, $rmax) = @_;
+  my (@x, @y);
+  ## use string to generate arrays in Ifeffit
+  return \@x, \@y;
+};
+
+sub histogram_gamma {
+  my ($self, $string, $rmin, $rmax) = @_;
+  my (@x, @y);
+  ## use string to generate arrays in Ifeffit
+  return \@x, \@y;
+};
+
+
+1;
