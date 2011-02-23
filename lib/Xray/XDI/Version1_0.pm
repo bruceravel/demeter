e0978e8b95ad609f1ff304eeea5494806b2ebf5d
(no author) <(no author)@a48c6eca-4e1b-0410-a7b3-d802d4c683d2>
2011-02-23 17:48:39 +0000

    use EOL correctly in XDI grammer
    git-svn-id: http://cars9.uchicago.edu/svn/demeter/trunk@848 a48c6eca-4e1b-0410-a7b3-d802d4c683d2

--- lib/Xray/XDI/Version1_0.pm
+++ lib/Xray/XDI/Version1_0.pm
@@ -3,6 +3,9 @@ package Xray::XDI::Version1_0;
 use Moose::Role;
 use MooseX::Aliases;
 
+use vars qw($debug);
+$debug = 0;
+
 has 'version'	         => (is => 'rw', isa => 'Str', default => q{1.0});
 
 has 'applications'	 => (is => 'rw', isa => 'Str', default => q{});
@@ -65,40 +68,41 @@ has 'order' 	   => (is => 'rw', isa => 'ArrayRef',
 
 sub define_grammar {
   return <<'_EOGRAMMAR_';
-XDI: VERSION(?) FIELDS(?) COMMENTS(?) LABELS(?) DATA
+XDI: <skip: qr/[ \t]*/> VERSION(?) FIELDS(?) COMMENTS(?) LABELS(?) DATA
 
 UPALPHA:    /[A-Z]+/
 LOALPHA:    /[a-z]+/
 ALPHA:      /[a-zA-Z]+/
 DIGIT:      /[0-9]/
-INTEGER:    /\d+/
 WORD:       /[-a-zA-Z0-9_]+/
 PROPERWORD: /[a-zA-Z][-a-zA-Z0-9_]+/
 NOTDASH:    /^[#;][ \t]*(?!-+)/
 ## including # in ANY is problematic
-ANY:        /[^\#;\n]+/
+ANY:        /[^\#; \t\n\r]+/
 COMM:       /^[\#;]/
 
 CR:         /\n/
 LF:         /\r/
 CRLF:       CR LF
-EOL:        CRLF | CR | LF
+#EOL:        CRLF | CR | LF
+EOL:        /[\n\r]+/
 SP:         / \t/
 WS:         SP(s)
 TEXT:       WORD
 MATH:       /(?:ln)?[-+\*\$\/\(\)\d]+/
 EXPRESSION: WORD | MATH | SP
 
-SIGN:       /[-+]/
-EXPONENT:   /[eEdD]/  SIGN(?)  INTEGER
+#SIGN:       /[-+]/
+INTEGER:    /\d+/
+#EXPONENT:   /[eEdD]/  SIGN(?)  INTEGER
 #NUMBER:     DIGIT(s)  ("."  DIGIT(s))(?)  EXPONENT(?)
-INF:        /inf/i
-NAN:        /nan/i
-FLOAT:      /[+-]?\ *(\d+(\.\d*)?|\.\d+)([eE][+-]?\d+)?/  # see perlretut
+#INF:        /inf/i
+#NAN:        /nan/i
+FLOAT:      /[+-]?\ *(\d+(\.\d*)?|\.\d+)([eEdD][+-]?\d+)?/  # see perlretut
 ## what about nan and inf?
 
-FIELD_END:  COMM  /\/+/
-HEADER_END: COMM  /-{2,}/
+FIELD_END:  COMM  /\/+/ EOL
+HEADER_END: COMM  /-{2,}/ EOL
 
 XDI_VERSION:   "XDI/"  INTEGER  "."  INTEGER 
 ## this action is not quite right, indeed specifying a version format is probably a bad idea
@@ -106,114 +110,132 @@ APPLICATIONS:  WORD  "/"   INTEGER  ("." INTEGER)(s) {
   	        $Xray::XDI::object->applications(join("", @item[1..3]).'.'.join('.', @{$item[4]}));
 	        1;
 	       }
-VERSION: COMM XDI_VERSION  APPLICATIONS(s?)
+VERSION: COMM XDI_VERSION  APPLICATIONS(s?) EOL
 
 #CUT:            DIGIT(3)
 REFLECTION:     /\d{3}/
-ELEMENT:        ("Si" | "Ge" | "Diamond" | "YB66" | "InSb" | "Beryl" | "Multilayer")
-DATE:           /\d{4}-\d{2}-\d{2}/
-TIME:           /\d{2}:\d{2}:\d{2}/
+MATERIAL:       ("Si" | "Ge" | "Diamond" | "YB66" | "InSb" | "Beryl" | "Multilayer")
+DATETIME:       /\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}/
 HARMONIC_VALUE: /\d{1,2}/
 
 ABSCISSA:    COMM  "Abscissa"    ":"  MATH {
+             print(join("~", @item), $/) if $Xray::XDI::Version1_0::debug;
 	     $Xray::XDI::object->abscissa(join(" ", $item[4]));
             }
 
 BEAMLINE:    COMM  "Beamline"           ":"  TEXT(s) {
+             print(join("~", @item), $/) if $Xray::XDI::Version1_0::debug;
 	     $Xray::XDI::object->beamline(join(" ", @{$item[4]}));
             }
 
 COLLIMATION: COMM  "Collimation"        ":"  TEXT(s) {
+             print(join("~", @item), $/) if $Xray::XDI::Version1_0::debug;
 	     $Xray::XDI::object->collimation(join(" ", @{$item[4]}));
             }
 
-CRYSTAL:     COMM  "Crystal"            ":"  ELEMENT REFLECTION {
+CRYSTAL:     COMM  "Crystal"            ":"  MATERIAL REFLECTION {
+             print(join("~", @item), $/) if $Xray::XDI::Version1_0::debug;
 	     $Xray::XDI::object->crystal(join(" ", $item[4], $item[5]));
             }
 
 DSPACING:    COMM  "D_spacing"          ":"  FLOAT {
+             print(join("~", @item), $/) if $Xray::XDI::Version1_0::debug;
 	     $Xray::XDI::object->d_spacing(join(" ", @{$item[4]}));
             }
 
 EDGEENERGY:  COMM  "Edge_energy"        ":"  FLOAT {
+             print(join("~", @item), $/) if $Xray::XDI::Version1_0::debug;
 	     $Xray::XDI::object->edge_energy(join(" ", $item[4]));
             }
 
-ENDTIME:     COMM  "End_time"           ":"  DATE TIME {
-	     $Xray::XDI::object->end_time(join(" ", @{$item[4]}));
+ENDTIME:     COMM  "End_time"           ":"  DATETIME {
+             print(join("~", @item), $/) if $Xray::XDI::Version1_0::debug;
+	     $Xray::XDI::object->end_time(join(" ", $item[4]));
             }
 
 FOCUSING:    COMM  "Focusing"           ":"  TEXT(s) {
+             print(join("~", @item), $/) if $Xray::XDI::Version1_0::debug;
 	     $Xray::XDI::object->focusing(join(" ", @{$item[4]}));
             }
 
 HARMONIC:    COMM  "Undulator_harmonic" ":"  HARMONIC_VALUE {
+             print(join("~", @item), $/) if $Xray::XDI::Version1_0::debug;
 	     $Xray::XDI::object->undulator_harmonic(join(" ", $item[4]));
             }
 
 MUFLUOR:     COMM  "Mu_fluorescence"    ":"  MATH {
+             print(join("~", @item), $/) if $Xray::XDI::Version1_0::debug;
 	     $Xray::XDI::object->mu_fluorescence(join(" ", $item[4]));
             }
 
 MUREF:       COMM  "Mu_reference"       ":"  MATH {
+             print(join("~", @item), $/) if $Xray::XDI::Version1_0::debug;
 	     $Xray::XDI::object->mu_reference(join(" ", $item[4]));
             }
 
 MUTRANS:     COMM  "Mu_transmission"    ":"  MATH {
+             print(join("~", @item), $/) if $Xray::XDI::Version1_0::debug;
 	     $Xray::XDI::object->mu_transmission(join(" ", $item[4]));
             }
 
 REJECTION:   COMM  "Harmonic_rejection" ":"  TEXT(s) {
+             print(join("~", @item), $/) if $Xray::XDI::Version1_0::debug;
 	     $Xray::XDI::object->harmonic_rejection(join(" ", @{$item[4]}));
             }
 
 RINGCURRENT: COMM  "Ring_current"       ":"  FLOAT {
+             print(join("~", @item), $/) if $Xray::XDI::Version1_0::debug;
 	     $Xray::XDI::object->ring_current(join(" ", $item[4]));
             }
 
 RINGENERGY:  COMM  "Ring_energy"        ":"  FLOAT {
+             print(join("~", @item), $/) if $Xray::XDI::Version1_0::debug;
 	     $Xray::XDI::object->ring_energy(join(" ", $item[4]));
             }
 
-STARTTIME:   COMM  "Start_time"         ":"  DATE TIME {
-	     $Xray::XDI::object->start_time(join(" ", $item[4], $item[5]));
+STARTTIME:   COMM  "Start_time"         ":"  DATETIME {
+             print(join("~", @item), $/) if $Xray::XDI::Version1_0::debug;
+	     $Xray::XDI::object->start_time(join(" ", $item[4]));
             }
 
 SOURCE:      COMM  "Source"             ":"  TEXT(s) {
+             print(join("~", @item), $/) if $Xray::XDI::Version1_0::debug;
 	     $Xray::XDI::object->source(join(" ", @{$item[4]}));
             }
 
 
-FIELD_NAME:      PROPERWORD
 EXT_FIELD_NAME:  PROPERWORD
-
-EXT_FIELD:  COMM  EXT_FIELD_NAME  ":"  ANY(s?)  {
+EXT_FIELD:  COMM  EXT_FIELD_NAME  ":"  ANY(s?)  EOL {
+             print(join("~", @item), $/) if $Xray::XDI::Version1_0::debug;
              $Xray::XDI::object->push_extension($item[2] . ': '. join(" ", @{$item[4]}));
             }
-FIELD_LINE: DEFINEDFIELDS 
 
+FIELD_LINE: DEFINEDFIELDS
 DEFINEDFIELDS: (  ABSCISSA      | BEAMLINE    | CRYSTAL
                 | COLLIMATION   | DSPACING    | EDGEENERGY
                 | ENDTIME       | FOCUSING    | HARMONIC
                 | REJECTION     | MUFLUOR     | MUREF
                 | MUTRANS       | RINGCURRENT | RINGENERGY
                 | STARTTIME     | SOURCE
-               )
+               ) EOL
 
 FIELDS:  (FIELD_LINE | EXT_FIELD)(s) FIELD_END
 
-COMMENT_LINE: NOTDASH  ANY(s?)  {
+COMMENT_LINE: NOTDASH  ANY(s?) EOL {
+             print(join("~", @item), $/) if $Xray::XDI::Version1_0::debug;
              $Xray::XDI::object->push_comment(join(" ", @{$item[2]}));
             }
 COMMENTS:     COMMENT_LINE(s)  HEADER_END
 
-LABEL:    PROPERWORD
-LABELS:   COMM  PROPERWORD(s)  {
+LABEL:    ANY
+LABELS:   COMM  LABEL(s) EOL {
+             print(join("~", @item), $/) if $Xray::XDI::Version1_0::debug;
              $Xray::XDI::object->push_label(@{$item[2]});
             }
 
-DATA_LINE: <skip: qr/[ \t]*/> FLOAT(s?) /[\n\r]/ {
-             $Xray::XDI::object->add_data_point(@{$item[2]})  if $#{$item[2]}>-1;
+DATA_LINE: FLOAT(s) EOL {
+             print(join("~", @item), $/) if $Xray::XDI::Version1_0::debug;
+             $Xray::XDI::object->add_data_point(@{$item[1]})  if $#{$item[1]}>-1;
              #print join("~", "DATA_LINE", @{$item[2]}), $/;
             }
 DATA:      DATA_LINE(s?)
@@ -302,6 +324,14 @@ This grammer is expressed in BNF form as:
   blah blah
 
 =head1 BUGS AND LIMITATIONS
+
+=over 4
+
+=item *
+
+INF and NAN are not supported in this implementation
+
+=back
 
 Please report problems to Bruce Ravel (bravel AT bnl DOT gov)
 
