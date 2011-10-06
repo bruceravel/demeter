aa3e27e759c6f9ba1d0feba0731de9d958f3a372
Bruce Ravel <bravel@bnl.gov>
2011-10-06 10:27:24 -0400

    The element symbol type constraint was missing Am through Cf

--- lib/Demeter/StrTypes.pm
+++ lib/Demeter/StrTypes.pm
@@ -133,8 +133,8 @@ use vars qw(@element_list $element_regexp);
 		   sr y zr nb mo tc ru rh pd ag cd in sn sb te i xe cs
 		   ba la ce pr nd pm sm eu gd tb dy ho er tm yb lu hf
 		   ta w re os ir pt au hg tl pb bi po at rn fr ra ac
-		   th pa u np pu
-		   nu);
+		   th pa u np pu am cm bk cf
+		   nu); # es fm md no lr
 $element_regexp = Regexp::Assemble->new()->add(@element_list)->re;
 subtype Element,
   as Str,
