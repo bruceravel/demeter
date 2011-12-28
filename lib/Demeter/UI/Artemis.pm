79839bcae0d92126716982fcc6ef2b0650f1fda7
Bruce Ravel <bravel@bnl.gov>
2011-12-28 15:47:06 -0500

    Fixed a spurious report of a missing space group symbol in Artemis Updated MANIFEST

--- lib/Demeter/UI/Artemis.pm
+++ lib/Demeter/UI/Artemis.pm
@@ -1101,7 +1101,7 @@ sub make_feff_frame {
       return;
     };
   } else {
-    #$frames{$fnum}->{Atoms}->{used} = 0;
+    $frames{$fnum}->{Atoms}->{used} = 0;
     $frames{$fnum}->{Atoms}->{name}->SetValue('new');
     if ($file ne $BLANK) {
       # $frames{$fnum}->{notebook}->DeletePage(0);
