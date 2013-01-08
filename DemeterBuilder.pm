434869080f1ddcf8acdfbbc2d8aa24f28459c512
Bruce Ravel <bruce@lachesis.bnl.gov>
2013-01-08 10:17:38 -0500

    Work on Build script to install artug reliably

--- DemeterBuilder.pm
+++ DemeterBuilder.pm
@@ -162,6 +162,10 @@ sub ACTION_post_build {
 			   flatten => 1);
 };
 
+
+################################################################################
+### Manage documents
+
 ######
 ## need to manage moving images across for DPG and ARTUG
 ######
@@ -213,8 +217,10 @@ sub ACTION_copy_artug {
   chdir 'doc/artug/';
   #do 'build_dpg.PL';
   mkdir 'html' if not -d 'html';
-  system(q(./configure 1));
-  system(q(./bin/build));
+  #system(q(./configure 1));
+  $self->dispatch("configure_artug");
+  #system(q(./bin/build));
+  $self->dispatch("ttree_artug");
   chdir $here;
   mkdir 'blib/lib/Demeter/UI/Artemis/share/artug';
   dircopy('doc/artug/html', 'blib/lib/Demeter/UI/Artemis/share/artug/html') or die "symlink failed: $!";
@@ -232,6 +238,57 @@ sub ACTION_copy_images {
   };
 };
 
+sub ACTION_configure_artug {
+  my $self = shift;
+  my $here = cwd;
+  chdir 'doc/artug' if (cwd !~ m{artug});
+  my $id = cwd;
+
+  print "Configuring the Artemis User's Guide build system for your machine\n" if ($self->verbose);
+
+  print "  configuring bin directory\n" if ($self->verbose);
+  foreach my $b (qw(build)) { # tex texbw pod mobile
+    unlink File::Spec->catfile('bin', $b);
+    system("tpage --define installdir=$id bin/$b.tt > bin/$b");
+    chmod 0755, File::Spec->catfile('bin', $b);
+  };
+
+  print "  configuring etc directory\n" if ($self->verbose);
+  foreach my $b (qw(ttree)) { #  ttree_tex ttree_texbw ttree_pod ttree_mobile
+    unlink File::Spec->catfile('etc', $b);
+    system("tpage --define installdir=$id etc/$b.tt > etc/$b.cfg");
+  };
+
+  print "  copying map and version templates\n" if ($self->verbose);
+  copy(File::Spec->catfile('etc', 'map'),  File::Spec->catfile('templates', 'lib', 'config'));
+  copy(File::Spec->catfile('etc', 'version'),  File::Spec->catfile('templates', 'lib', 'config'));
+
+  if (($^O eq 'MSWin32') or ($^O eq 'cygwin')) {
+    print "  copying perl modules from DPG\n" if ($self->verbose);
+    dircopy(File::Spec->catfile('..', 'dpg', 'Template'), 'Template');
+  } else {
+    print "  linking to perl modules from DPG\n" if ($self->verbose);
+    symlink('../dpg/Template', 'Template');
+  };
+
+  chdir $here;
+};
+
+sub ACTION_ttree_artug {
+  my $self = shift;
+  my $here = cwd;
+  chdir 'doc/artug' if (cwd !~ m{artug});
+  my $id = cwd;
+  my $verbose = q{};
+  $verbose = "--verbose", if ($self->verbose);
+  system("ttree -f $id/etc/ttree.cfg --relative --trim $verbose -a");
+  chdir $here;
+};
+
+
+################################################################################
+### Manage org-mode pages
+
 sub ACTION_org2html {
   if (not is_older("todo.org", File::Spec->catfile($ghpages, 'todo.html'))) {
     system('emacs --batch --eval "(setq org-export-headline-levels 2)" --visit=todo.org --funcall org-export-as-html-batch');
@@ -242,6 +299,10 @@ sub ACTION_org2html {
     move('Changes.html', File::Spec->catfile($ghpages, 'Changes.html'));
   };
 };
+
+
+################################################################################
+### Manage programming documentation
 
 sub ACTION_doctree {
   my $self = shift;
@@ -283,6 +344,10 @@ sub ACTION_touch_wrapper {
   mkpath(File::Spec->catfile($ENV{HOME}, 'perl', 'auto', 'Ifeffit')) if not -e File::Spec->catfile($ENV{HOME}, 'perl', 'auto', 'Ifeffit');
   copy(File::Spec->catfile('src', 'Ifeffit.so'), File::Spec->catfile($ENV{HOME}, 'perl', 'auto', 'Ifeffit', 'Ifeffit.so'));
 };
+
+
+################################################################################
+### Manage versioning
 
 sub ACTION_bump {
   my $self = shift;
