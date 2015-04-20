a598cea35a090b95d734ea088085d8f065062ce5
Bruce Ravel <bravel@bnl.gov>
2015-04-20 14:37:08 -0400

    a few tweaks

--- lib/Demeter/Data/Mu.pm
+++ lib/Demeter/Data/Mu.pm
@@ -465,7 +465,6 @@ sub edgestep_error {
 sub autobk {
   my ($self) = @_;
   my $group = $self->group;
-#      print ">>>", $self->datatype, "  ", $self->update_bkg, $/;
 
   if ($self->datatype eq 'detector') {
     carp($self->name . " is a detector group, which cannot have its background removed\n\n");
