24b6f66561c3d324a4277069761b047c20af74db
Bruce Ravel <bravel@bnl.gov>
2016-09-19 21:46:34 -0400

    fix some things for webatoms

--- lib/Demeter/Project.pm
+++ lib/Demeter/Project.pm
@@ -37,6 +37,7 @@ requires 'is_windows';
 
 sub dot_folder {
   my ($self) = @_;
+  return q{} if ($ENV{DEMETER_MODE} eq 'web');
   my $folder = ($self->is_windows)
     ? File::Spec->catfile($ENV{APPDATA}, "demeter")
       : File::Spec->catfile($ENV{HOME}, ".horae");
