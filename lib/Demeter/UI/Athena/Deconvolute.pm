d2ce40aabad6413f7de99bb096bb6b57f9811b42
BasedOnTechnology <therobotist@gmail.com>
2021-12-12 17:03:37 -0500

    Deconvolution panel larch test works

--- lib/Demeter/UI/Athena/Deconvolute.pm
+++ lib/Demeter/UI/Athena/Deconvolute.pm
@@ -11,10 +11,8 @@ use Scalar::Util qw(looks_like_number);
 
 #use Demeter::UI::Wx::SpecialCharacters qw(:all);
 
-#xas_deconvolve(energy, norm=None, group=None, form='lorentzian',
-
 use vars qw($label);
-$label = "Deconvolute data";	# used in the Choicebox and in status bar messages to identify this tool
+$label = "Convolute and add noise to data";	# used in the Choicebox and in status bar messages to identify this tool
 
 my $tcsize = [60,-1];
 
@@ -26,6 +24,19 @@ sub new {
   $this->{sizer}  = $box;
 
   my $gbs = Wx::GridBagSizer->new( 5, 5 );
+
+  if($ENV{DEMETER_BACKEND} ne 'larch'){
+    $box->Add(Wx::StaticText->new($this, -1, "This part of Athena has not yet been implemented."), 0, wxALL|wxALIGN_CENTER_HORIZONTAL, 5);
+
+    $box->Add(1,1,1);		# this spacer may not be needed, Journal.pm, for example
+
+    $this->{document} = Wx::Button->new($this, -1, 'Document section: deconvolution');
+    $box -> Add($this->{document}, 0, wxGROW|wxALL, 2);
+    EVT_BUTTON($this, $this->{document}, sub{  $app->document("process.deconv")});
+
+    $this->SetSizerAndFit($box);
+    return $this;
+  }
 
   $gbs->Add(Wx::StaticText->new($this, -1, 'Group'),                         Wx::GBPosition->new(0,0));
   $gbs->Add(Wx::StaticText->new($this, -1, 'Convolution function'),          Wx::GBPosition->new(1,0));
@@ -81,8 +92,19 @@ sub pull_values {
 
 ## this subroutine fills the controls when an item is selected from the Group list
 sub push_values {
+  if($ENV{DEMETER_BACKEND} ne 'larch'){
+    return;
+  }
+
   my ($this, $data) = @_;
-  1;
+  $this->{group}->SetLabel($data->name);
+  $this->Enable(1);
+  $this->{make}->Enable(0);
+  if ($data->datatype eq 'chi') {
+    $this->{function}->Enable(0);
+    $this->{width}->Enable(0);
+    return;
+  };
 };
 
 ## this subroutine sets the enabled/frozen state of the controls
@@ -91,16 +113,74 @@ sub mode {
   1;
 };
 
-## yes, there is some overlap between what push_values and mode do.
-## This separation was useful in Main.pm.  Some of the other tools
-## make mode a null op.
+sub get_values {
+  if($ENV{DEMETER_BACKEND} ne 'larch'){
+    return;
+  }
+
+  my ($this) = @_;
+  my $function = ($this->{function}->GetSelection) ? 'lorentzian' : 'gaussian';
+  my $width    = $this->{width}->GetValue || 0;
+  my $noise    = $this->{noise}->GetValue || 0;
+
+  if (not looks_like_number($width)) {
+    $::app->{main}->status("Not plotting -- your value for the width is not a number!", 'error|nobuffer');
+    return ($function, $width, $noise, 0);
+  };
+  if ($width < 0) {
+    $this->{width}->SetValue(0);
+    $width = 0;
+  };
+  if (not looks_like_number($noise)) {
+    $::app->{main}->status("Not plotting -- your value for the noise is not a number!", 'error|nobuffer');
+    return ($function, $width, $noise, 0);
+  };
+  if ($noise < 0) {
+    $this->{noise}->SetValue(0);
+    $noise = 0;
+  };
+  return ($function, $width, $noise, 1);
+};
+
+sub plot {
+  my ($this, $data) = @_;
+  my $busy = Wx::BusyCursor->new();
+  $::app->{main}->{PlotE}->pull_single_values;
+  $data->po->set(e_mu=>1, e_markers=>1, e_bkg=>0, e_pre=>0, e_post=>0, e_norm=>0, e_der=>0, e_sec=>0, e_i0=>0, e_signal=>0);
+  my ($function, $width, $noise, $ok) = $this->get_values($data);
+  return if not $ok;
+  $data->po->start_plot;
+  $data -> plot('E');
+  $this->{processed}  = $data -> Clone(name=>sprintf("%s: %.2f eV %s, %.3f noise", $data->name, $width, ucfirst($function), $noise));
+  $this->{processed} -> convolve(width=>$width, type=>$function) if ($width > 0);
+  $this->{processed} -> noise(noise=>$noise, which=>'xmu') if ($noise > 0);
+  $this->{processed} -> plot('E');
+  $this->{make}->Enable(1);
+  $::app->{main}->status(sprintf("Plotted %s with convolution and/or added noise", $data->name));
+  $::app->heap_check(0);
+  undef $busy;
+};
+
+sub make {
+  my ($this, $app) = @_;
+
+  my $index = $app->current_index;
+  if ($index == $app->{main}->{list}->GetCount-1) {
+    $app->{main}->{list}->AddData($this->{processed}->name, $this->{processed});
+  } else {
+    $app->{main}->{list}->InsertData($this->{processed}->name, $index+1, $this->{processed});
+  };
+  $app->{main}->status(sprintf("Convolved and/or added noise to %s and made a new data group", $app->current_data->name));
+  $app->modified(1);
+  $app->heap_check(0);
+};
 
 1;
 
 
 =head1 NAME
 
-Demeter::UI::Athena::Deconvolute - A deconvolution tool for Athena
+Demeter::UI::Athena::ConvoluteNoise - A convolution and artficial noise tool for Athena
 
 =head1 VERSION
 
@@ -108,7 +188,8 @@ This documentation refers to Demeter version 0.9.26.
 
 =head1 SYNOPSIS
 
-This module provides a
+This module provides a tool for convolving mu(E) data and adding
+artifical noise to mu(E) or chi(k) data.
 
 =head1 CONFIGURATION
 
